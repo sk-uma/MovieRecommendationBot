81ba462b7b4bc17e5a20fee23cc673ce7453f801
TAKU1101 <taku.kuma1101@icloud.com>
2021-09-26 21:54:38 +0900

    Add: get ranking

---
+++ srcs/scraping_box_office_revenue.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+import io
+import datetime
+import pandas as pd
+
+def get_dateURL(year, month, date):
+    return 'https://mimorin2014.blog.fc2.com/blog-date-{year:4d}{month:2d}{date:2d}.html'.format(year=year, month=month, date=date)
+
+def get_ranking(year, month, date):
+    res = requests.get(get_dateURL(year, month, date))
+    soup = BeautifulSoup(res.text, 'html.parser')
+    tags = soup.find_all(class_="content entry grid_content")
+    for tag in tags:
+        if 'id' in tag.attrs.keys():
+            entry_body = tag.find(class_="entry_body")
+            entry_body.p.decompose()
+            parse_text = str(entry_body).replace('<br/>', '\n').replace('*', '').replace('　', ',').splitlines()[3:-2]
+            df = pd.read_csv(io.StringIO('\n'.join(parse_text)), header=None, usecols=[0, 6], names=['rank', 'title'])
+            return df
+
+def get_latest_ranking():
+    now = datetime.datetime.now()
+    return get_ranking(now.year, now.month, now.date - 1)
+
+get_ranking(2020, 9, 26)
