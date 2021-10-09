595df6f21e85eae7e9cda3234724b671d6f665e1
TAKU1101 <taku.kuma1101@icloud.com>
2021-10-09 07:50:29 +0200

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+cd /root/chatBot && \
 python3 /root/chatBot/srcs/scraping_box_office_revenue.py > /dev/null 2> /dev/null
 
 echo '0 0 * * * python3 /root/chatBot/srcs/scraping_box_office_revenue.py > /dev/null 2> /dev/null' > cron.conf
